66b3215d992139c486fba7d65da34af594b95f1c
quysnt <hello.quynt@gmail.com>
2025-03-17 18:21:24 +0700

    html page about 80%

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,14 @@ $(document).ready(function () {
     $(".slick-dots li").eq(nextSlide).find(".progress").addClass("active"); // Kích hoạt progress bar mới
   });
 
+  $('.js-slider-review').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+  });
+
   $(".js-btn-show-menu").click(function (e) {
     e.preventDefault();
     $(this).toggleClass("is-active");
@@ -41,7 +49,7 @@ $(document).ready(function () {
   });
 
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 400) {
+    if ($(this).scrollTop() > 100) {
       $(".js-header").addClass("is-fixed");
     } else {
       $(".js-header").removeClass("is-fixed");
