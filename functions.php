98df96e20bf3b530b1d5da62602247881c9fbd2d
quysnt <hello.quynt@gmail.com>
2025-03-19 00:15:08 +0700

    update logo, color banner MV, hidden section blogs

--- functions.php
+++ functions.php
@@ -132,7 +132,6 @@ function remove_category_base()
 }
 remove_category_base();
 
-
 function get_current_language()
 {
   $current_url = $_SERVER['REQUEST_URI'];
@@ -157,11 +156,11 @@ function get_language_urls($slug)
   return array('en' => $en_url, 'vi' => $vi_url);
 }
 
-function get_current_language() {
-  $current_url = $_SERVER['REQUEST_URI'];
-  if (preg_match('/^\/en\//', $current_url)) {
-      return array('short' => 'en', 'full' => 'en-US');
-  } else {
-      return array('short' => 'vi', 'full' => 'vi-VN');
-  }
-}
+// function get_current_language() {
+//   $current_url = $_SERVER['REQUEST_URI'];
+//   if (preg_match('/^\/en\//', $current_url)) {
+//       return array('short' => 'en', 'full' => 'en-US');
+//   } else {
+//       return array('short' => 'vi', 'full' => 'vi-VN');
+//   }
+// }
